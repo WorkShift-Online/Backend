1893ede1981d6e06c3f8e42b49d42f7e62fc41ed
Maxim Ustin (aka mxustin) <mxustin@gmail.com>
2023-12-16 00:52:44 +0300

    Тестовый коммит

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return ('👋 Привет! Это — публичный API 🕗 WorkShift Backend. '
+    return ('👋 Привет! Это — публичный API 🕗 WorkShift Backend. Если вы '
+            'видите эту надпись, значит всё ✅ работает!'
             '📚 Документация доступна по адресу: '
             'https://api.workshift.online/docs')
